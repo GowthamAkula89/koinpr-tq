35bd48bec5e0ce7ffeb1741d583d96a989f58e19
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-06-12 01:24:44 +0530

    Add HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {DataProvider} from './Components/DataContext';
+import HomePage from './Pages/HomePage';
 function App() {
   return (
     <div className="App">
       <DataProvider>
-        
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />}/>
+          </Routes>
+        </Router>
       </DataProvider>
     </div>
   );

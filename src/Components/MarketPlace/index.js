63d79f8fb4056b2e36453d6253d42bfe1610c2da
Akula Gowtham <gowtham.akula@udtrucks.com>
2024-06-14 11:02:37 +0530

    Update MarketPlace

--- src/Components/MarketPlace/index.js
+++ src/Components/MarketPlace/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 import "./marketPlace.css";
 import OfferingCard from "../OfferingCard";
@@ -7,8 +7,10 @@ import {config} from "../../App"
 import { Link } from "react-router-dom";
 const MarketPlace = () => {
     const {offeringsData, setOfferingsData} = useContext(DataContext);
+    const [loading, setLoading] = useState(false);
     useEffect (() =>{
         const fetchData = async() => {
+            setLoading(true);
             try {
               const response = await fetch(config.endpoint);
               const data = await response.json();
@@ -17,32 +19,39 @@ const MarketPlace = () => {
             catch (error) {
                 console.error('Error fetching projects:', error);
             }
+            finally{
+                setLoading(false);
+            }
           }
         fetchData()
       },[])
       console.log(offeringsData);
     return(
-        <div className="marketplace-container">
-            <div className="marketplace-header">
-                <div className="marketplace-title">Koinpr Marketplace</div>
-                <div className="marketplace-search-add">
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search" className="marketplace-search"/>
-                        <IoMdSearch className="search-btn"/>
+        <>
+        {loading ? (<div className="loading-message">Loading please wait...</div>):(
+            <div className="marketplace-container">
+                <div className="marketplace-header">
+                    <div className="marketplace-title">Koinpr Marketplace</div>
+                    <div className="marketplace-search-add">
+                        <div className="search-bar">
+                            <input type="text" placeholder="Search" className="marketplace-search"/>
+                            <IoMdSearch className="search-btn"/>
+                        </div>
+                        <Link to="/addoffering" className="nav-link">
+                            <div className="marketplace-add-btn">Add Offering</div>
+                        </Link>
                     </div>
-                    <Link to="/addoffering" className="nav-link">
-                        <div className="marketplace-add-btn">Add Offering</div>
-                    </Link>
                 </div>
+                <div className="go-to-cart-btn">Go to Cart</div>
+                <div className="offerings-container">
+                    {offeringsData.map((data) => (
+                        <div key={data._id}><OfferingCard data={data}/></div>
+                    ))}
+                </div>
+                
             </div>
-            <div className="go-to-cart-btn">Go to Cart</div>
-            <div className="offerings-container">
-                {offeringsData.map((data) => (
-                    <div key={data._id}><OfferingCard data={data}/></div>
-                ))}
-            </div>
-             
-        </div>
+        )}
+        </>
     )
 }
 export default MarketPlace;

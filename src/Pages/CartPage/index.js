1d149bac587ee39397a9c801f1f2da103288ec3c
Akula Gowtham <gowtham.akula@udtrucks.com>
2024-06-14 14:35:01 +0530

    Add Responsiveness

--- src/Pages/CartPage/index.js
+++ src/Pages/CartPage/index.js
@@ -1,11 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import Header from "../../Components/Header";
 import CartSection from "../../Components/CartSection";
-import DataContext from "../../Components/DataContext";
 const CartPage = () => {
-    const {cart} =useContext(DataContext);
-    const data = cart[0];
-    console.log(data);
+    
     return(
         <div>
             <Header/>
